e99be1b92a08daae269509a9523ebf468c91ff1b
Konstantin Balchikov <kbalchikov@outlook.com>
2021-01-16 14:31:13 +0500

    Module02 homework solution

--- Module02-AsyncBasics/ThreadPoolExercises.Core/ThreadingHelpers.cs
+++ Module02-AsyncBasics/ThreadPoolExercises.Core/ThreadingHelpers.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace ThreadPoolExercises.Core
 {
@@ -12,8 +13,35 @@ namespace ThreadPoolExercises.Core
             // * In a loop, check whether `token` is not cancelled
             // * If an `action` throws and exception (or token has been cancelled) - `errorAction` should be invoked (if provided)
 
+            for (int i = 0; i < repeats; i++)
+            {
+                if (token.IsCancellationRequested)
+                {
+                    errorAction?.Invoke(new TaskCanceledException());
+                    return;
+                }
 
+                Exception? threadException = null;
+                var thread = new Thread(() =>
+                {
+                    try
+                    {
+                        action();
+                    }
+                    catch (Exception exc)
+                    {
+                        threadException = exc;
+                    }
+                });
+                thread.Start();
+                thread.Join();
 
+                if (threadException != null)
+                {
+                    errorAction?.Invoke(threadException);
+                    return;
+                }
+            }
         }
 
         public static void ExecuteOnThreadPool(Action action, int repeats, CancellationToken token = default, Action<Exception>? errorAction = null)
@@ -23,8 +51,42 @@ namespace ThreadPoolExercises.Core
             // * In a loop, check whether `token` is not cancelled
             // * If an `action` throws and exception (or token has been cancelled) - `errorAction` should be invoked (if provided)
 
+            for (int i = 0; i < repeats; i++)
+            {
+                if (token.IsCancellationRequested)
+                {
+                    errorAction?.Invoke(new TaskCanceledException());
+                    return;
+                }
 
+                var autoResetEvent = new AutoResetEvent(false);
+                Exception? threadException = null;
 
+                ThreadPool.QueueUserWorkItem(_ =>
+                {
+                    autoResetEvent.Reset();
+                    try
+                    {
+                        action();
+                    }
+                    catch (Exception exc)
+                    {
+                        threadException = exc;
+                    }
+                    finally
+                    {
+                        autoResetEvent.Set();
+                    }
+                });
+
+                autoResetEvent.WaitOne();
+
+                if (threadException != null)
+                {
+                    errorAction?.Invoke(threadException);
+                    return;
+                }
+            }
         }
     }
 }
