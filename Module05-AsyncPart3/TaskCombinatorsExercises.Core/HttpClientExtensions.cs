73d639b7eab36c47a25345a906ad533bee29c005
Konstantin Balchikov <kbalchikov@outlook.com>
2021-03-13 22:42:54 +0500

    Module05 homework solution

--- Module05-AsyncPart3/TaskCombinatorsExercises.Core/HttpClientExtensions.cs
+++ Module05-AsyncPart3/TaskCombinatorsExercises.Core/HttpClientExtensions.cs
@@ -23,7 +23,30 @@ namespace TaskCombinatorsExercises.Core
         public static async Task<string> ConcurrentDownloadAsync(this HttpClient httpClient,
             string[] urls, int millisecondsTimeout, CancellationToken token)
         {
-            return String.Empty;
+            using (var cts = new CancellationTokenSource())
+            using (var linkedCts = CancellationTokenSource.CreateLinkedTokenSource(token, cts.Token))
+            {
+                var delayTask = Task.Delay(millisecondsTimeout, token);
+                var requests = Task.WhenAny(urls.Select(x => GetHttpResponseAsync(httpClient, x, linkedCts.Token)));
+                var first = await Task.WhenAny(delayTask, requests);
+
+                if (first == delayTask)
+                {
+                    cts.Cancel();
+                    throw new TaskCanceledException();
+                }
+
+                var firstRequest = await requests;
+                string response = await firstRequest;
+                cts.Cancel();
+                return response;
+            }
+        }
+
+        private static async Task<string> GetHttpResponseAsync(HttpClient httpClient, string url, CancellationToken token)
+        {
+            var response = await httpClient.GetAsync(url, token);
+            return await response.Content.ReadAsStringAsync();
         }
     }
 }
